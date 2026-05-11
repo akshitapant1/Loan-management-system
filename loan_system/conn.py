5fe4164a3f8669ca9cc1f9db7788dc2877c208e3
akshitapant1 <122249943+akshitapant1@users.noreply.github.com>
2026-05-11 23:14:36 +0545

    Delete loan_system directory

--- loan_system/conn.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-#define connection and cursor
-connection =sqlite3.connect('dbname')
-cursor = connection.cursor()
-#create table 
-command1="""CREATE TABLE IF NOT EXISTS 
-loan(loan_id INTEGER PRIMARY KEY,amount INTEGER)"""
-cursor.execute(command1)
